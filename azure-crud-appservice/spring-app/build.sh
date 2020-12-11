2721a99e8dae2fea049b01e1d9de74c4bb63b1b5
uonyekwuluje <uionyekwuluje@gmail.com>
2020-12-11 05:05:36 -0500

    updating docker compose

--- azure-crud-appservice/spring-app/build.sh
+++ azure-crud-appservice/spring-app/build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-docker network create crdapp-network
-
+echo "Create Database Image"
+echo "......................"
 cd mysql-setup/
 docker build -t mysql57-data .
-sleep 5
-#docker run --name appdatabase --network crdapp-network -p 3308:3306 -e MYSQL_ROOT_PASSWORD=mysqlpassword -e MYSQL_USER=appadmin -e MYSQL_PASSWORD=mysqlpassword -e MYSQL_DATABASE=testdb -d mysql57-data
-docker run --name appdatabase --network crdapp-network -p 3308:3306 -e MYSQL_ROOT_PASSWORD=mysqlpassword -e MYSQL_USER=appadmin -e MYSQL_PASSWORD=mysqlpassword -d mysql57-data
-
 cd ..
 sleep 10
-echo "Create Package"
-echo "..............."
+echo "Create Application Image"
+echo "........................."
 ./mvnw clean package -Dmaven.test.skip=true
 sleep 2
 docker build -t springcrudapp .
 sleep 4
-docker run --name springdbapp -p 5000:8080 --network crdapp-network springcrudapp 
+docker-compose up
